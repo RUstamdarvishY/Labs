c717774f7c916f72dbbd31414f911240d5550e61
rustam <darvishi123@yandex.ru>
2023-01-03 18:14:12 +0300

    lab7

--- lab7/7.4.2.cpp
+++ lab7/7.4.2.cpp
@@ -1,85 +1,128 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <vector>
 
 using namespace std;
 
-void initialize_file(string file_name)
+string initialize_file()
+{
+    string file_name;
+    cout << "Enter file name: ";
+    cin >> file_name;
+
+    fstream file;
+    file.open(file_name, ios::out);
+
+    if (file.is_open())
+    {
+        cout << "File created" << endl;
+
+        file << "фамилия,группа,физика,математика,информатика,средняя,\n";
+        file.close();
+    }
+    return file_name;
+}
+
+void read_students(string file_name)
 {
     fstream file;
     file.open(file_name, ios::in);
 
     if (file.is_open())
     {
-        file << "фамилия,группа,физика,информатика,математика,средняя," << endl;
+        string str;
+        while (!file.eof())
+        {
+            getline(file, str);
+            if (str.empty())
+                continue;
+            cout << str << endl;
+        }
         file.close();
     }
-}
+};
 
-struct Students
+void add_student(string file_name)
 {
     string surname;
     int group_number;
     int physics_grade;
-    int informatics_grade;
     int math_grade;
+    int informatics_grade;
     double average_grade;
 
-    void read_students(string file_name){
-        fstream file;
-        file.open(file_name, ios::out);
+    fstream file;
+    file.open(file_name, ios::app);
 
-        if (file.is_open()){
-            string str;
-            while (!file.eof()) 
-            {
-                getline(file, str); 
-                if (str.empty()) continue; 
-                cout << str << endl;
-            }
-            file.close();
-        }
-    };
-    
-    void add_student(string file_name){
-        fstream file;
-        file.open(file_name, ios::app);
+    if (file.is_open())
+    {
+        cout << "Enter student surname: ";
+        cin >> surname;
+        cout << "Enter group number: ";
+        cin >> group_number;
+        cout << "Enter physics grade: ";
+        cin >> physics_grade;
+        cout << "Enter math grade: ";
+        cin >> math_grade;
+        cout << "Enter informatics grade: ";
+        cin >> informatics_grade;
+        average_grade = static_cast<double>(physics_grade + math_grade + informatics_grade) / 3;
 
-        if (file.is_open()){
-            
-        }
+        file << surname << "," << group_number << "," << physics_grade << "," << math_grade << "," << informatics_grade << "," << average_grade << "," << endl;
 
-
-    };
-
-    
+        cout << "Student added" << endl;
+    }
 };
 
+void get_students_with_low_grades(string file_name)
+{
+    fstream file;
+    file.open(file_name, ios::out);
 
-void write_students_to_file(string file_name){
-        fstream file;
-        file.open(file_name, ios::in);
+    string group_number;
+    cout << "Enter group number" << endl;
+    cin >> group_number;
 
-        if (file.is_open()){
-            for ()
-            file <<  << ; 
-            file.close(); 
+    int passing_grade = 4;
+
+    vector<vector<string>> student_info;
+    if (file.is_open())
+    {
+        int j;
+        while (!file.eof())
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                getline(file, student_info[j][i], ',');
+                if (student_info[j].empty())
+                    continue;
+            }
+            j++;
+        }
+        file.close();
+    }
+    for (int i = 0; i < student_info.size(); i++)
+    {
+        if (student_info[i][1] == group_number)
+        {
+            if (stoi(student_info[i][2]) < passing_grade || stoi(student_info[i][3]) < passing_grade || stoi(student_info[i][4]) < passing_grade)
+            {
+                cout << "Student with low grade: " << student_info[i][0] << endl;
+            }
         }
     }
-
-void get_students_with_low_grades(){
-
-    };
-
-
+};
 
 int main()
 {
-    string file_name;
-    cout << "Enter file name in *.csv format: ";
-    cin >> file_name;
 
-    initialize_file(file_name);
+    // initialize_file();
+
+    add_student("student.csv");
+    cout << "\n";
+    read_students("student.csv");
+    cout << "\n";
+    get_students_with_low_grades("student.csv");
 
     return 0;
 }
